fe7354d95d11647ad88a02dd506d38a8a67bd20d
lotusntp <lotusnatthaphorn@gmail.com>
2023-07-30 01:08:41 +0700

    update api

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup java -jar ./api-roblox.jar -webAllowOthers -tcpAllowOthers > ./log.txt 2>&1 &
+nohup java -jar ./api-roblox.jar > ./log.txt 2>&1 &
 # nohup ./mvnw spring-boot:run > log.txt 2>&1 &
 echo $! > ./pid.file
